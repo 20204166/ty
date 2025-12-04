67123e817112c1d7987be57f6dca6681e564474d
Bekithemba <115995846+20204166@users.noreply.github.com>
2025-12-04 16:34:20 +0000

    Update training_text_summarization.py

--- training_text_summarization.py
+++ training_text_summarization.py
@@ -1315,7 +1315,7 @@ def warm_start_from_old_model(model, old_model_path):
 
     print(f"✅ Warm-start finished: copied weights for {copied} layers, skipped {skipped}.")
 
-def train_model(data_path, epochs=35, batch_size=16, emb_dim=64, train_from_scratch=False, phase="all"):
+def train_model(data_path, epochs=4, batch_size=16, emb_dim=64, train_from_scratch=False, phase="all"):
     inputs, targets = load_training_data(data_path)
     split = int(0.9 * len(inputs))
     save_dir = "app/models/saved_model"
@@ -1425,7 +1425,7 @@ def train_model(data_path, epochs=35, batch_size=16, emb_dim=64, train_from_scra
         configure_trainable_for_phase(model, phase)
 
         base_opt = Adam(
-            learning_rate=1e-5,
+            learning_rate=3e-6,
             global_clipnorm=1.0,  # gradient clipping
         )
         opt = base_opt
